a626ddcf57c21600e84462694e21eb650e9a2a87
jean saindrenan <j.saindrenan62@gmail.com>
2024-04-02 10:10:39 +0200

    morpion  finis

--- React-learn/React-Learn/src/App.jsx
+++ React-learn/React-Learn/src/App.jsx
@@ -1,17 +1,90 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import "@fortawesome/fontawesome-free/css/all.css";
+import React, { useState } from "react";
+import "./App.css";
+
 function App() {
-return (
-<>
-<div>
-<input type="checkbox" checked />
-<p>Nom de tâche 1</p>
-<i class="fa-solid fa-pen"></i>
-        <i class="fa-solid fa-trash"></i>
-<button className="btn btn-primary">X</button>
-</div>
-</>
-)
+  const [pions, setPions] = useState(Array(9).fill(""));
+  const [joueurs] = useState(["X", "O"]);
+  const [tour, setTour] = useState(0);
+  const [jeuEstFini, setJeuEstFini] = useState(false);
+  const [message, setMessage] = useState(
+    "Le jeu peut commencer ! Joueur " + joueurs[tour] + " c'est votre tour."
+  );
+
+  function estValide(index) {
+    return pions[index] === "";
+  }
+
+  function rechercherVainqueur(pions, joueurs, tour) {
+    const lignesGagnantes = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let ligne of lignesGagnantes) {
+      const [a, b, c] = ligne;
+      if (
+        pions[a] === joueurs[tour] &&
+        pions[b] === joueurs[tour] &&
+        pions[c] === joueurs[tour]
+      ) {
+        setJeuEstFini(true);
+        setMessage(
+          "Le joueur " +
+            joueurs[tour] +
+            " a gagné ! Rejouer"
+        );
+        return true;
+      }
+    }
+
+    if (pions.every((pion) => pion !== "")) {
+      setJeuEstFini(true);
+      setMessage("Match Nul ! Rejouer");
+      return true;
+    }
+
+    return false;
+  }
+
+  function handleClick(index) {
+    if (jeuEstFini || !estValide(index)) {
+      setMessage(
+        "Case occupée ! Joueur " +
+          joueurs[tour] +
+          " c'est toujours à vous !"
+      );
+      return;
+    }
+
+    const nouveauxPions = [...pions];
+    nouveauxPions[index] = joueurs[tour];
+    setPions(nouveauxPions);
+
+    if (!rechercherVainqueur(nouveauxPions, joueurs, tour)) {
+      setTour((tour + 1) % 2);
+      setMessage("Joueur " + joueurs[(tour + 1) % 2] + " c'est à vous !");
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Le jeu du morpion</h1>
+      <div id="Jeu">
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
+          <button key={index} onClick={() => handleClick(index)}>
+            {pions[index]}
+          </button>
+        ))}
+      </div>
+      <div id="StatutJeu" dangerouslySetInnerHTML={{ __html: message }} />
+    </div>
+  );
 }
+
 export default App;
--- React-learn/React-Learn/src/main.jsx
+++ React-learn/React-Learn/src/main.jsx
@@ -7,7 +7,6 @@ import "./App.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <p className={app["text-green"]}>Hello green !</p>
-    <p className="text-red">Hello red !</p>
+    
   </React.StrictMode>
 );
